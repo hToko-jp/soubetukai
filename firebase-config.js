e79a7396d07c1c1c81cbe73c34f16a209ce1c582
hTokoro <tokoro.h@gmail.com>
2026-03-09 08:53:33 +0900

    日付変更２

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyC2rXePcYuaIa7ZW-f1O0gHfxujGQk8UAU",
-    authDomain: "chat-app-50f39.firebaseapp.com",
-    projectId: "chat-app-50f39",
-    storageBucket: "chat-app-50f39.firebasestorage.app",
-    messagingSenderId: "76027706381",
-    appId: "1:76027706381:web:4ec30f5d22d3d9c1cda944",
-    measurementId: "G-3F544RXRBT",
-    databaseURL: "https://chat-app-50f39-default-rtdb.asia-southeast1.firebasedatabase.app"
+    apiKey: "AIzaSyAS17Tid2DxyncPcYxgJ3Wh800JwGjIm20",
+    authDomain: "soubetukai.firebaseapp.com",
+    projectId: "soubetukai",
+    storageBucket: "soubetukai.firebasestorage.app",
+    messagingSenderId: "1019778751519",
+    appId: "1:1019778751519:web:e4dbf67d4d109522bac92a",
+    measurementId: "G-JJPPQ62WRC",
+    databaseURL: "https://soubetukai-default-rtdb.asia-southeast1.firebasedatabase.app"
 };
